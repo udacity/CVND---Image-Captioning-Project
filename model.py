7c3ea87709931d0c7ecbb2164c523d9be9cdc787
volfi <31245915+volfi@users.noreply.github.com>
2019-04-28 20:50:43 +0200

    update changes

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-
+import torch.nn.functional as F
+from vocabulary import Vocabulary
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
@@ -22,12 +23,94 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
+    #def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+
+        #super(DecoderRNN, self).__init__()
+        #self.hidden_dim = hidden_size
+        #self.embed = nn.Embedding(vocab_size, embed_size)
+        #self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        #self.fc = nn.Linear(hidden_size, vocab_size)
+        #self.hidden = (torch.zeros(1, 1, hidden_size),torch.zeros(1, 1, hidden_size))
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super().__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        ## TODO: define the LSTM
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+
+        ## TODO: define the final, fully-connected output layer
+        self.fc = nn.Linear(hidden_size, vocab_size)
+        
+        # initialize the weights
+        #self.init_weights()
+        #self.hidden = (torch.zeros(1, 1, hidden_size),torch.zeros(1, 1, hidden_size))
+        self.softmax = nn.Softmax(dim=2)
+        
+    def gforward(self, features, captions):
+        
+        try:
+            caption_emb = self.embed(captions[:,:-1])
+            embeddings = torch.cat((features.unsqueeze(1), caption_emb), 1)
+            print('hi')
+        except:
+            embeddings = features
+            print('ho')
+        print(embeddings.shape)
+        lstm_out, self.hidden = self.lstm(embeddings)
+        
+        output = self.fc(lstm_out)
+
+        return output
+
+    def gsample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        #feature_vec = EncoderCNN.forward(inputs)
+        out_list = []
+        for i in range(max_len):
+            print('iteration', i)
+            output, states = self.lstm(inputs, states)
+            #inputs_act = self.hidden
+            outputs = self.fc(output.squeeze(1))
+            target_index = outputs.max(1)[1]
+            print(target_index) 
+            out_list.append(target_index.item())
+            inputs = self.embed(target_index).unsqueeze(1)
+        return out_list
     
+        
     def forward(self, features, captions):
-        pass
+
+        cap_embedding = self.embed(captions[:,:-1])
+        embeddings = torch.cat((features.unsqueeze(1), cap_embedding), 1)              
+        lstm_out, self.hidden = self.lstm(embeddings, None)
+        outputs = self.fc(lstm_out)
+
+        return outputs
+   
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+
+        outlist = []
+
+        for i in range(max_len):
+
+            outputs, states = self.lstm(inputs, states)
+            #print(outputs.squeeze(1).shape)
+            outputs = self.fc(outputs.squeeze(1))
+            #print(outputs.max(1)[1])
+            target_index = outputs.max(1)[1]
+            outlist.append(target_index.item())
+            inputs = self.embed(target_index).unsqueeze(1)
+
+        return outlist
+    
+    def init_weights(self):
+        ''' Initialize weights for fully connected layer '''
+        initrange = 0.1
+        
+        # Set bias tensor to all zeros
+        self.fc.bias.data.fill_(0)
+        # FC weights as random uniform
+        self.fc.weight.data.uniform_(-1, 1)
