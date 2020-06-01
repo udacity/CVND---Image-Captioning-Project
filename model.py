9e8075b03d365858f0e37bc4ac8660bd62986211
Houssam <houssam.naous1@gmail.com>
2020-06-01 12:14:13 +0000

    added and tested decoder model

--- model.py
+++ model.py
@@ -19,14 +19,24 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
+
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        captions = self.embed(captions[:, :-1])
+        features = features.unsqueeze(1)
+        
+        x = torch.cat((features, captions), 1)
+        x, (h, c) = self.lstm(x)
+        x = self.fc(x)
+        return x
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
