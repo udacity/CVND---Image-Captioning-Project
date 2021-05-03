7fc8d92f9e706421a05fecb0df2f2c9c35614c26
Ubuntu <houssam@learny2.vgob25sudlmubahi13c21ugrub.bx.internal.cloudapp.net>
2021-05-03 13:55:24 +0000

    final update

--- download_data.sh
+++ download_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir datak
-cd datak
+mkdir data
+cd data
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 wget http://images.cocodataset.org/zips/train2014.zip
 wget http://images.cocodataset.org/zips/test2014.zip
 wget http://images.cocodataset.org/annotations/image_info_test2014.zip
 unzip annotations_trainval2014.zip
 unzip train2014.zip
-unzip val2014.zip
+unzip test2014.zip
 unzip image_info_test2014.zip
 rm *.zip
